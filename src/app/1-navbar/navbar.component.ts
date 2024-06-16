05191ec6a137561419692ae8c1101cc93d343024
Deyvid-10 <marmolejodeyvid@gmail.com>
2024-06-16 00:28:33 -0400

    .

--- src/app/1-navbar/navbar.component.ts
+++ src/app/1-navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
   import { Router, NavigationEnd } from '@angular/router';
 import { DataService } from '../data.service';
 import { ManageVariableService } from '../manage-variable.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -25,27 +26,52 @@ export class NavbarComponent {
     )
   }
 
-  // Inyecta los servicios de manejo de variable
+  // Inyecta los servicios de manejo de variable, la data de los articulo y el FormBuilder
   private manageVariable = inject (ManageVariableService)
   private data = inject (DataService)
+  private fb = inject (FormBuilder)
 
-  // actualiza la cantidad de articulos en el carrito
+  // Variable para la cantidad de articulos en el carrito
   quatityOnCart: number = 0
+
+  // Creación del formulario
+  searchForm: FormGroup = this.fb.group({})
 
   ngOnInit()
   {
+    // actualiza la cantidad de articulos en el carrito
     this.manageVariable.updateQuantity().subscribe(value => {
       this.quatityOnCart  = value
     })
 
     this.manageVariable.updateDetail(this.data.updateArticlesCart())
+
+    // Configura los parametros del formulario
+    this.searchForm = this.fb.group({
+      search: ["", Validators.required]
+    })
+  }
+
+  // Obtiene el valor del formulaario
+  getSearch(): void
+  {
+    console.log(this.searchForm.value);
+    
   }
 
   // Función para el menú desplegable
-  isMenu: boolean = true
+  showMenu: boolean = true
 
   menu(): void
   {
-    this.isMenu = !this.isMenu
+    this.showMenu = !this.showMenu
+  }
+
+  // Función para visualizar el buscador
+  showSearch: boolean = false
+
+  search(): void
+  {
+    this.showSearch = !this.showSearch
   }
 }
